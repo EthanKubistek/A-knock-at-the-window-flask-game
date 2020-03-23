4da680a74f219bc437f54470185a4b9e7c9e38ed
Ethan Kubistek <ekubistek@gmail.com>
2020-03-23 15:09:53 -0400

    adding to flaskherokudemo's __init__.py and adding to test

---
+++ tests/test_flaskherokudemo.py
@@ -0,0 +1,9 @@
+from flaskherokudemo import create_app
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+def test_inded(client):
+    response = client.get('/')
+    assert response.data == b'Flask Heroku Demo'

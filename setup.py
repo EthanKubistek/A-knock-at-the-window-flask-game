745a25c3278516ba65d740b09a9b2135eb766edb
Ethan Kubistek <ekubistek@gmail.com>
2020-03-23 12:51:41 -0400

    Finished the setup for the Flask Adventure Game and have the hello.py running

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="Flask Adventure Game"
+    version="0.0.1",
+    author="Ethan Kubistek",
+    author_email="ekubistek@email.com",
+    url="https://github.com/yourusername/yourproject",
+    description="Flask Adventure Game OOP game",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    extras_require=[],
+    tests_require=['pytest'],
+    python_requires='>=3.6',
+)

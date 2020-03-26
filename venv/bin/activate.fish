22f8424d9a2039e4ebecfc6074f7373bb8864bbf
Ethan Kubistek <ekubistek@gmail.com>
2020-03-26 16:53:43 -0400

    fixed html problems with speeling

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ethankubistek/Desktop/Python/Flask_Adventure_Game/venv"
+set -gx VIRTUAL_ENV "/Users/ethankubistek/Desktop/Python/Flask-Window/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
